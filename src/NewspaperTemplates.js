a2212800ecc970f398de95fccc7a73904e337607
Dominika Dziubińska <dziubinska.dominika@wp.pl>
2020-10-22 22:53:46 +0200

    initial commit

---
+++ src/NewspaperTemplates.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NewspaperTemplates = () => {
+  return (
+      <h3>NewspaperTemplates</h3>
+  );
+};
+
+export default NewspaperTemplates;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import NewspaperTemplates from './NewspaperTemplates';
+
+render(<NewspaperTemplates />, document.getElementById('root'));
